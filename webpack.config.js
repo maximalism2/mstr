1ceb49a64175f6ff144342d73cf9dfb016422b5f
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-09 02:15:58 +0300

    Update webpack.config. Remove ExtractTextPlugin. I don't need it.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: __dirname,
@@ -10,22 +9,21 @@ module.exports = {
   entry: {
     app: ['webpack-dev-server/client?http://localhost:8000',
     'webpack/hot/only-dev-server',
-    './clients_source/'],
+    './clients_source/index'],
   },
   output: {
     path: path.join(__dirname, '/public/js'),
     filename: '[name].js',
     publicPath: '/js/'
   },
-  resolve: {
-    extensions: ['', '.jsx', '.js', '.json'],
-    modulesDirectories: [
-      'node_modules',
-      path.resolve(__dirname, './node_modules')
-    ]
-  },
+  // resolve: {
+  //   extensions: ['', '.jsx', '.js', '.json'],
+  //   modulesDirectories: [
+  //     'node_modules',
+  //     path.resolve(__dirname, './node_modules')
+  //   ]
+  // },
   plugins: [
-    new ExtractTextPlugin('../styles/[name].css', { allChunks: true }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
@@ -35,12 +33,8 @@ module.exports = {
   module: {
     loaders: [{
       test: /(\.js|\.jsx)$/,
-      loaders: ['react-hot', 'babel?presets[]=es2015&presets[]=react'],
+      loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'clients_source')
-    }, {
-        test: /(\.scss|\.css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&postcss!sass?sourceMap'),
-        include: path.join(__dirname, './public/styles')
     }]
   }
 };
