12ac4deaa44607b1a380665d4f9f34452453b400
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-09 02:16:43 +0300

    Create login form

--- clients_source/index.js
+++ clients_source/index.js
@@ -4,14 +4,38 @@ import { render } from 'react-dom';
 class App extends Component {
   render() {
     return (
-      <div className="center main-container">
-        <h1 className="title is-1">Need to run</h1>
-        <code className="npm-code">
-          npm i --save
-            history
-            babel-polyfill
-            moment
-        </code>
+      <div className="container main-container">
+        <h1 className="title is-3 main-title">Maestro <small>(Alpha)</small></h1>
+        <div className="column is-4 is-offset-4 form-wrapper">
+          <p className="login-input">
+            <input
+              className="input"
+              type="text"
+              placeholder="email or username"
+            />
+          </p>
+          <p className="login-input">
+            <input
+              className="input"
+              type="password"
+              placeholder="password"
+            />
+          </p>
+          <button className="button is-primary">Login</button>
+          {" "}
+          <a className="button is-link sign-up-link">Sign up</a>
+        </div>
+        <footer className="footer">
+          <div className="container">
+            <div className="content is-centered">
+              <p>
+                <strong>Maestro</strong> by <a href="http://github.com/necinc">Max Vyznyuk</a>. The source code is licensed
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+              </p>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
