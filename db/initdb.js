331282363c7fa2f9111082a2773b792aac7a9a9a
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-12 02:02:01 +0300

    Update init database module

--- db/initdb.js
+++ db/initdb.js
@@ -1,15 +1,23 @@
+/**
+ * Module exports function, which return database's
+ * models prepared to using;
+ */
+
 const mongoose = require('mongoose');
 const origin = 'localhost';
 const dbName = 'maestro';
 const Schema = mongoose.Schema;
 
-const priceSchema = require('./priceModelTemplate');
-const productSchema = require('./productModelTemplate');
+const priceTemplate = require('./templates/priceModelTemplate');
+const productTemplate = require('./templates/productModelTemplate');
+
+const priceSchema = new Schema(priceTemplate);
+const productSchema = new Schema(productTemplate);
 
 function initdb() { // Initialize the database
   var connection = mongoose.createConnection(origin, dbName);
-  const Price = connection.model('Prices', new Schema(priceSchema));
-  const Product = connection.model('Product', new Schema(productSchema));
+  const Price = connection.model('Prices', priceSchema);
+  const Product = connection.model('Product', productSchema);
 
   return {
     Price: Price,
