eab562e3f60de991d1d910c7d7e4a5b6dbb880c7
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-12 02:01:00 +0300

    Pass model templates to specific folder

---
+++ db/templates/priceModelTemplate.js
@@ -0,0 +1,14 @@
+/**
+ * Template for document "Prices" in database
+ */
+const Schema = require('mongoose').Schema;
+
+const template = {
+  name: String,                      // Name of price (catalog)
+  discount: Number,                  // Discount for specific price
+  products: [Schema.Types.ObjectId], // Array of products IDs
+  priceId: Schema.Types.ObjectId,    // Id of specific price
+  updatedAt: Date                    // Date of last updating (modifying)
+};
+
+module.exports = template;
