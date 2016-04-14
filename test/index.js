b4b6f91e4f87db9ba831ba8b5b8dea409ead7a49
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-15 01:09:35 +0300

    Try to write tests for db, failed

--- test/index.js
+++ test/index.js
@@ -1,5 +1,15 @@
 var assert = require('assert');
 
+// describe('Database', function() {
+//   describe('Price', function() {
+//     describe('.create()', function() {
+//       it('should return false when passed {} (empty object)', function() {
+//         assert.equal(false, )
+//       });
+//     });
+//   });
+// });
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when it include 2', function () {
