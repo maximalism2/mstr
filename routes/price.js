01cd96d6b0e5178cba5831874264bdaa14779359
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-17 16:04:25 +0300

    Create new route for prices

--- routes/price.js
+++ routes/price.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var Price = require('../db/models/Prices');
+var Product = require('../db/models/Products');
 
 router.post('/', (req, res, next) => {
   req.on('error', err => { console.error(err); });
@@ -33,9 +34,20 @@ router.get('/', function(req, res, next) {
 router.get('/:id/', function(req, res, next) {
   Price.readById(req.params.id)
     .then(result => {
-      res.json(JSON.stringify(result));
-      res.end();
-    })
+      let price = result;
+      let query = {
+        priceOrigin: req.params.id
+      }
+      console.log(`\n Price => `, JSON.stringify(price), query);
+
+      Product.readWhere(query)
+        .then(result => {
+          console.log('result =>', result);
+          price.products = result;
+          res.json(JSON.stringify(price));
+          res.end();
+        });
+    });
 });
 
 module.exports = router;
