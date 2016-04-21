0c160180a59887413c38c2af6689eb99e80de10c
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-22 02:44:12 +0300

    Done the post method, which create a new price with own product list

--- routes/price.js
+++ routes/price.js
@@ -5,25 +5,40 @@ var Price = require('../db/models/Prices');
 var Product = require('../db/models/Products');
 
 router.post('/', (req, res, next) => {
-  req.on('error', err => { console.error(err); });
+  // Make copy of request body
+  const template = JSON.parse(JSON.stringify(req.body));
+  // Make copy of products (in the future they can be deleted)
+  const copyOfProductsTemplates = JSON.parse(JSON.stringify(template.products));
 
-  req.on('data', (chunk, we, fe) => {
-    let template = JSON.parse(chunk.toString());
-    console.log('TEMPLATE => ', template);
-    let result = Price.create(template);
-    if (result.error) {
-      res.json(JSON.stringify(result));
-      res.end();
-    } else {
-      result.then(createdPrice => {
-        res.json(JSON.stringify(createdPrice));
+  const result = Price.create(template);
+
+  if (result.error) {
+    res.json(JSON.stringify(result));
+    res.end();
+  } else {
+
+    result.then(data => {
+      // Make the new array of products, but now we add the required property
+      // originPrice, which is _id of created price
+      let productsTemplates = copyOfProductsTemplates.map(item => {
+        return Object.assign({}, item, {
+          priceOrigin: data._id
+        });
+      });
+
+      const resultOfProducts = Product.createOf(productsTemplates);
+
+      if (Array.isArray(resultOfProducts)) {
+        let newPrice = JSON.parse(JSON.stringify(data));
+        newPrice.products = resultOfProducts;
+        res.json(JSON.stringify(newPrice));
         res.end();
-      })
-    }
-  });
+      }
+    });
+  }
 });
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   Price.read()
     .then(result => {
       res.json(JSON.stringify(result));
@@ -31,22 +46,28 @@ router.get('/', function(req, res, next) {
     })
 });
 
-router.get('/:id/', function(req, res, next) {
+router.get('/:id/', (req, res, next) => {
+  //  Search the price, then looks for products, this links to this price,
+  //  get them and put to product property in price object
   Price.readById(req.params.id)
     .then(result => {
-      let price = result;
-      let query = {
-        priceOrigin: req.params.id
-      }
-      console.log(`\n Price => `, JSON.stringify(price), query);
+      if (result) {
+        let price = result;
+        let query = {
+          priceOrigin: req.params.id
+        }
+        console.log(`\n Price => `, JSON.stringify(price), query);
 
-      Product.readWhere(query)
+        Product.readWhere(query)
         .then(result => {
           console.log('result =>', result);
           price.products = result;
           res.json(JSON.stringify(price));
           res.end();
         });
+      } else {
+        res.end();
+      }
     });
 });
 
