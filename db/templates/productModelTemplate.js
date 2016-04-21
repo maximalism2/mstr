51a2ebc629f97113af41b24cbca5ce06bc55057a
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-22 02:43:04 +0300

    Fix typo in productModelTemplate

--- db/templates/productModelTemplate.js
+++ db/templates/productModelTemplate.js
@@ -7,7 +7,7 @@ const template = {
   name: String,                       // Name Of product
   cost: Number,                       // How much cost this product
   priceOrigin: Schema.Types.ObjectId, // From which catalog this product is
-  unitOfMeasurment: String,           // Unit of measurment
+  unitOfMeasurement: String,          // Unit of measurement
 };
 
 module.exports = template;
