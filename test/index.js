a75fc03a470383893658e4462f5e96a9aa35ac08
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-23 00:51:25 +0300

    Start writing tests for backend

--- test/index.js
+++ test/index.js
@@ -1,25 +1,13 @@
+'use strict';
 var assert = require('assert');
+var Price = require('../db/models/Prices');
 
-// var Price = require('../db/models/Prices');
-
-// describe('Database', function() {
-//   describe('Price', function() {
-//     describe('.create()', function() {
-//       try {
-//         Price.create({});
-//       } catch(e) {
-//         it('should return false when passed {} (empty object)', function() {
-//           assert.equal(false, e.)
-//         });
-//       }
-//     });
-//   });
-// });
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when it include 2', function () {
-      assert.equal(-1, [2, 4].indexOf(3));
-    });
+describe('Database', () => {
+  describe('.create()', () => {
+  describe('Price', () => {
+      it('should return object with error when passed {} (empty object)', () => {
+        assert.equal('string', typeof Price.create({}).error);
+      });
+    })
   });
 });
