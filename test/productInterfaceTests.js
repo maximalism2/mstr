27569aa39f53aeeb664fbbb1511f2e853015059a
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-23 20:39:23 +0300

    Write test to test

--- test/productInterfaceTests.js
+++ test/productInterfaceTests.js
@@ -1,5 +1,9 @@
 'use strict';
 var assert = require('assert');
-var Product = require('../db/models/Products');
 
-// describe('')
+var Product = require('../db/models/Products');
+describe('Test to test', () => {
+  it('test to test', () => {
+    assert.equal(1, 1);
+  });
+});
