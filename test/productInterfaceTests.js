dc9e813a70e91afee3c10533868c4cf4caa33f00
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-29 21:28:21 +0300

    Write tests for update and something else, start writing test for removing, Product's interface

--- test/productInterfaceTests.js
+++ test/productInterfaceTests.js
@@ -120,7 +120,7 @@ describe('Model interface', () => {
         assert.equal(true, isErrorObject(res));
       });
     });
-    describe('.create(), usually', () => {
+    describe('.create(), usually:', () => {
       it('should return an object with created price', () => {
         let instance = {
           name: 'some name',
@@ -282,6 +282,148 @@ describe('Model interface', () => {
         assert(true, Array.isArray(res));
       });
     });
-
+    describe('.readById(), must returns object with error when:', () => {
+      it('called without arguments', () => {
+        let res = Product.readById();
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed something except ObjectId', () => {
+        let query = 123;
+        let res = Product.readById(query);
+        assert.equal(true, isErrorObject(res));
+      });
+    });
+    describe('.readById(), usually:', () => {
+      it('should returns some object with product or null', () => {
+        let query = Types.ObjectId("57236095cbe7c2ce0e11bee5");
+        let res = Product.readById(query);
+        res.then(result => {
+          assert.equal(true, (result instanceof Object || result === null));
+        })
+      });
+    });
+    describe('.read(), usually:', () => {
+      it('should returns some array of product or null', () => {
+        let res = Product.read();
+        res.then(result => {
+          assert.equal(true, (Array.isArray(result) || result === null));
+        })
+      });
+    });
+    describe('.update(), must returns object with error when:', () => {
+      it('called without arguments', () => {
+        let res = Product.update();
+        assert.equal(true, isErrorObject(res));
+      });
+      it('called just by one argument', () => {
+        let res = Product.update('some id');
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed something except ObjectId in the first argument', () => {
+        let id = 123;
+        let query = {};
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but query is not an object', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = 'some string';
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but query is an empty object', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = {};
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but in the query not exists some of required fields', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = {
+          otherField: 'some value'
+        };
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but the name field in query is not a string', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = { name: 123 };
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but the name field in query is an empry string', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = { name: '' };
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but the name field in query is bigger than 300 characters', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = {
+          name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        };
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but the cost field in query is not a number', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = {
+          cost: 'some string'
+        };
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but the cost field in query is less than 0', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = {
+          cost: -1
+        };
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but the unitOfMeasurement field is not a string', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = {
+          unitOfMeasurement: 123
+        };
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed a valid id, but the unitOfMeasurement field is an empty string', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee5');
+        let query = {
+          unitOfMeasurement: ''
+        };
+        let res = Product.update(id, query);
+        assert.equal(true, isErrorObject(res));
+      });
+    });
+    describe('.update(), usually:', () => {
+      it('should return something', () => {
+        let id = Types.ObjectId('57236095cbe7c2ce0e11bee4');
+        let query = {
+          name: 'The new name',
+          cost: 69,
+          unitOfMeasurement: 'units'
+        }
+        let res = Product.update(id, query);
+        res.then(result => {
+          assert.equal(1,1);
+        }).catch(err => {
+          assert.equal(1,2);
+        });
+      });
+    });
+    describe('.remove(), must returns object with error when:', () => {
+      it('called without arguments', () => {
+        let res = Product.remove();
+        assert.equal(true, isErrorObject(res));
+      });
+      it('passed something except ObjectId', () => {
+        let query = 123;
+        let res = Product.remove(query);
+        assert.equal(true, isErrorObject(res));
+      });
+    });
   });
 });
