abd57db4766f1feaea2d592d12b385971b6bdedc
Max Vyznyuk <mvyznyuk@gmail.com>
2016-04-30 23:18:36 +0300

    Create separated file for loader

---
+++ clients_source/common/components/loader.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import { TransitionMotion, spring } from 'react-motion';
+
+class Loader extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+Loader.propTypes = {
+  // showed: PropTypes.boolean.isRequired,
+}
