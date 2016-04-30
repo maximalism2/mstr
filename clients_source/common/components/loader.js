5d40a18e02b4ee9c8325619c52738bd90f8afeee
Max Vyznyuk <mvyznyuk@gmail.com>
2016-05-01 00:04:52 +0300

    Try to create animated loader

--- clients_source/common/components/loader.js
+++ clients_source/common/components/loader.js
@@ -2,13 +2,42 @@ import React, { Component, PropTypes } from 'react';
 import { TransitionMotion, spring } from 'react-motion';
 
 class Loader extends Component {
+  willLeave() {
+    return {
+      opacity: spring(0)
+    };
+  }
+
+  getStyles() {
+    return {
+      key: 'main-loader',
+      style: {
+        opacity: 1
+      }
+    }
+  }
+
   render() {
     return (
-
+      <TransitionMotion
+        willLeave={this.willLeave}
+        styles={this.getStyles}
+      >
+        {interpolatedStyle => (
+          <div
+            className="spinner"
+            style={{
+              opacity: interpolatedStyle.style.opacity
+            }}
+          ></div>
+        )}
+      </TransitionMotion>
     );
   }
 }
 
 Loader.propTypes = {
-  // showed: PropTypes.boolean.isRequired,
+  showed: PropTypes.bool.isRequired
 }
+
+export default Loader
