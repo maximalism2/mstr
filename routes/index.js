45a2e8e6c354607441711e473bff9cf6d682a054
Max Vyznyuk <mvyznyuk@gmail.com>
2016-05-02 11:38:42 +0300

    Remove needless comment

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-// router.get(/(\/|\/price\/|\/prices\/|\/prices\/new\/)/, function(req, res, next) {
 router.get('/*+/', function(req, res, next) {
   let pathToIndex = path.resolve(__dirname, '../public/index.html');
   res.sendFile(pathToIndex);
