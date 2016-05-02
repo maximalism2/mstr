bfc95133d80593806bf893517e8535f358282c3a
Max Vyznyuk <mvyznyuk@gmail.com>
2016-05-02 23:45:47 +0300

    Global content container style and other small changes

--- clients_source/routes/price/components/Content.js
+++ clients_source/routes/price/components/Content.js
@@ -10,6 +10,76 @@ class Content extends Component {
         </div>
         <div className="products-list content-container">
         </div>
+
+        <table className="table content-container">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Instrument</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Misty Abbott</td>
+              <td>Bass Guitar</td>
+              <td className="is-link is-icon">
+                <a href="#">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </td>
+              <td className="is-link is-icon">
+                <a href="#">
+                  <i className="fa fa-instagram"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>John Smith</td>
+              <td>Rhythm Guitar</td>
+              <td className="is-link is-icon">
+                <a href="#">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </td>
+              <td className="is-link is-icon">
+                <a href="#">
+                  <i className="fa fa-instagram"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Robert Mikels</td>
+              <td>Lead Guitar</td>
+              <td className="is-link is-icon">
+                <a href="#">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </td>
+              <td className="is-link is-icon">
+                <a href="#">
+                  <i className="fa fa-instagram"></i>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Karyn Holmberg</td>
+              <td>Drums</td>
+              <td className="is-link is-icon">
+                <a href="#">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </td>
+              <td className="is-link is-icon">
+                <a href="#">
+                  <i className="fa fa-instagram"></i>
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
       </div>
     );
   }
