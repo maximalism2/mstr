9cc77e1b74daeacf06611ef7fffbccb8f7f7dee8
Max Vyznyuk <mvyznyuk@gmail.com>
2016-05-03 03:08:15 +0300

    Create 'alpha' version of single price view

--- clients_source/routes/price/components/Content.js
+++ clients_source/routes/price/components/Content.js
@@ -2,11 +2,11 @@ import React, { Component, PropTypes } from 'react';
 
 class Content extends Component {
   render() {
-    console.log('price content props', this.props);
+    let { data } = this.props;
     return (
       <div className="price-content container">
         <div className="main-info content-container">
-          <h1 className="title price-title">{this.props.data.name}</h1>
+          <h1 className="title price-title">{data.name}</h1>
         </div>
         <div className="products-list content-container">
         </div>
@@ -14,72 +14,23 @@ class Content extends Component {
         <table className="table content-container">
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Instrument</th>
-              <th></th>
-              <th></th>
+              <th>Назва</th>
+              <th>Одиниці вимірювання</th>
+              <th>Ціна</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Misty Abbott</td>
-              <td>Bass Guitar</td>
-              <td className="is-link is-icon">
-                <a href="#">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </td>
-              <td className="is-link is-icon">
-                <a href="#">
-                  <i className="fa fa-instagram"></i>
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>John Smith</td>
-              <td>Rhythm Guitar</td>
-              <td className="is-link is-icon">
-                <a href="#">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </td>
-              <td className="is-link is-icon">
-                <a href="#">
-                  <i className="fa fa-instagram"></i>
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>Robert Mikels</td>
-              <td>Lead Guitar</td>
-              <td className="is-link is-icon">
-                <a href="#">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </td>
-              <td className="is-link is-icon">
-                <a href="#">
-                  <i className="fa fa-instagram"></i>
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>Karyn Holmberg</td>
-              <td>Drums</td>
-              <td className="is-link is-icon">
-                <a href="#">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </td>
-              <td className="is-link is-icon">
-                <a href="#">
-                  <i className="fa fa-instagram"></i>
-                </a>
-              </td>
-            </tr>
+            {data.products.map((product, index) => {
+              return (
+                <tr key={index}>
+                  <td>{product.name}</td>
+                  <td>{product.unitOfMeasurement}</td>
+                  <td>{product.cost}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
-
       </div>
     );
   }
