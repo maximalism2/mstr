b450c76bbf570d1838484634eb7bec710a1231fc
Max Vyznyuk <mvyznyuk@gmail.com>
2016-05-17 15:20:36 +0300

    Get out the getCarretPosition function to helpers

--- clients_source/routes/price/components/Content.js
+++ clients_source/routes/price/components/Content.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { findDOMNode } from 'react-dom';
 import Loader from '../../../common/components/loader';
+import { getCaretPosition } from '../../../common/helpers/';
 import cnames from 'classnames';
 
 import { diff } from 'deep-diff';
@@ -14,34 +15,6 @@ var rhs = {
   name: 'dd'
 }
 
-function getCaretPosition(oField) {
-
-  // Initialize
-  var iCaretPos = 0;
-
-  // IE Support
-  if (document.selection) {
-
-    // Set focus on the element
-    oField.focus();
-
-    // To get cursor position, get empty selection range
-    var oSel = document.selection.createRange();
-
-    // Move selection start to 0 position
-    oSel.moveStart('character', -oField.value.length);
-
-    // The caret position is selection length
-    iCaretPos = oSel.text.length;
-  }
-
-  // Firefox support
-  else if (oField.selectionStart || oField.selectionStart == '0')
-    iCaretPos = oField.selectionStart;
-
-  // Return results
-  return iCaretPos;
-}
 
 class Input extends Component {
   constructor() {
@@ -51,7 +24,7 @@ class Input extends Component {
   }
 
   keyDownHanlder(e) {
-    console.log(e.which);
+    console.log(e.which, getCaretPosition(e.target));
   }
 
   componentDidMount() {
