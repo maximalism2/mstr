504ad83a27232a1a8c49faf07141f453420eb364
Max Vyznyuk <mvyznyuk@gmail.com>
2016-06-06 11:10:49 +0300

    Some changes, I forgot which they are

--- clients_source/index.js
+++ clients_source/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
-import AppContainer from './container/mainContainer';
+// import AppContainer from './container/mainContainer';
 import 'babel-polyfill';
 
 import configureStore from './store/';
@@ -14,23 +14,25 @@ const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
 var routes = {
-    childRoutes: [{
-      path: '/',
-      component: AppContainer,
-      // Temp
-      childRoutes: [
-        require('./routes/prices/'),
-        require('./routes/price/')
-      ]
-      // getChildRoutes(location, callback) {
-      //   require.ensure([], function(require) {
-      //     callback(null, [
-      //       require('./routes/prices'),
-      //       // And so one
-      //     ])
-      //   });
-      // }
-    },]
+  component: 'div',
+  childRoutes: [{
+    path: '/',
+    getComponent(location, callback) {
+      console.log('get component');
+      require.ensure([], function(require) {
+        callback(null, require('./container/mainContainer').default)
+      });
+    },
+    getChildRoutes(location, callback) {
+      require.ensure([], function(require) {
+        callback(null, [
+          require('./routes/prices'),
+          require('./routes/price/')
+          // And so one
+        ]);
+      });
+    }
+  }]
 }
 
 
